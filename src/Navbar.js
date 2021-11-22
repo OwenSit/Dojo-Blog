408e7bed1aa7e5924d4eb6325abeec8f7461eda4
Owen Xue <owensit98@gmail.com>
2021-11-21 22:49:12 -0500

    add inline style

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,16 @@ const Navbar = () => {
       <h1>Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a
+          href="/create"
+          style={{
+            color: "green",
+            backgroundColor: "yellow",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
